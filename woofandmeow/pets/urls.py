757015665ddd4ca63bc8b8a54405ae9c1989a3d5
ananthudev <ananthudevdv@gmail.com>
2024-06-25 09:52:37 +0530

    added html as partial views

--- woofandmeow/pets/urls.py
+++ woofandmeow/pets/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('dog-lookup/', views.dog_lookup, name='dog_lookup'),
+    path('cat-lookup/', views.cat_lookup, name='cat_lookup'),
 ]
--- woofandmeow/pets/views.py
+++ woofandmeow/pets/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pets/index.html')
+    return render(request, 'base.html')
+
+def dog_lookup(request):
+    return render(request, 'partialviews/dog.html')
+
+def cat_lookup(request):
+    return render(request, 'partialviews/cat.html')
+
