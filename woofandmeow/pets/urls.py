db094f9b8323f4fbedf617687185f68b8348d367
ananthudev <ananthudevdv@gmail.com>
2024-06-26 12:00:42 +0530

    added login authentication

--- woofandmeow/pets/urls.py
+++ woofandmeow/pets/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from .views import custom_login, custom_logout
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('dog-lookup/', views.dog_lookup, name='dog_lookup'),
     path('cat-lookup/', views.cat_lookup, name='cat_lookup'),
+    
+    #login and logout path
+    
+    path('login/', custom_login, name='login'),
+    path('logout/', custom_logout, name='logout'),
 ]
--- woofandmeow/pets/views.py
+++ woofandmeow/pets/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomLoginForm
 
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'base.html')
 
@@ -7,5 +11,28 @@ def dog_lookup(request):
     return render(request, 'partialviews/dog.html')
 
 def cat_lookup(request):
+    
     return render(request, 'partialviews/cat.html')
 
+
+
+#Login logic
+def custom_login(request):
+    if request.method == 'POST':
+        form = CustomLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                form.add_error(None, 'Invalid username or password')
+    else:
+        form = CustomLoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def custom_logout(request):
+    logout(request)
+    return redirect('login')
